0ed18019899c9b0be1960982033749c90dfe6e86
choco30 <47217764+choco30@users.noreply.github.com>
2022-03-16 23:30:59 +0530

    Create validate stack sequence.py

---
+++ validate stack sequence.py
@@ -0,0 +1,18 @@
+# leetcode 946
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        size=len(pushed)
+        left=0
+        stack=[]
+        right=0
+        for i in range(size):
+            stack.append(pushed[i])
+            if pushed[i]==popped[right]:
+                left=len(stack)-1
+                while left>=0 and stack[left]==popped[right]  and right<size:
+                    stack.pop()
+                    right+=1
+                    left-=1
+                print(stack)    
+        return True if len(stack)==0 else False
+                   
